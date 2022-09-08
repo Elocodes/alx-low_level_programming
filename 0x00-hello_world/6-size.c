af0eb52f2888b59c675efc4d8912d48cd8d52c48
Elocodes <elochukwuodoeze@gmail.com>
2022-09-08 14:29:57 -0700

    c changes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Size of a char: %d %c\n", sizeof(char));
+	printf("Size of an int: %d %c\n", sizeof(int));
+	printf("Size of a long int: %d %c\n", sizeof(long int));
+	printf("Size of a long long int: %d %c\n", sizeof(long long int));
+	printf("Size of a float: %d %c\n", sizeof(float));
+	
+	return 0;
+}
---
+++ 0x00-hello_world/Usingputs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*prints output using puts() */
+
+int main()
+{       
+	
+	char MyStr[] = "\"Programming is like building a multilingual puzzle";
+	
+	puts(MyStr);
+
+	return 0;
+}
