9530cb32da4bd24b448d26026fff10d0627bfba5
Elocodes <elochukwuodoeze@gmail.com>
2022-09-08 21:38:52 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{    	
-	char MyStr[] = "\"Programming is like building a multilingual puzzle";	
+{
+	char MyStr[] = "\"Programming is like building a multilingual puzzle\n";	
 	puts(MyStr);
 	return (0);
 }
