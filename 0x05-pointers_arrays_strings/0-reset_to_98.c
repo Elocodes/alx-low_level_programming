b79b85053394af441b9fa75f3f1e9727ef3bca02
Elocodes <elochukwuodoeze@gmail.com>
2022-09-19 21:07:13 -0700

    more on c functions

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - changes whatever value n has to 98 when called
+ *'n' - the initial integer value of n before the assignment of new number
+ *
+ * return is exempted because function returns nothing
+ */
+void reset_to_98(int *n)	/*'n' - the integer pointer */
+{
+	*n = 98;
+}
