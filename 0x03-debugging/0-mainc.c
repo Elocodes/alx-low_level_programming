b9b3cd6f5bdbee30d379e79da7aef995c4e1c4fc
Elocodes <elochukwuodoeze@gmail.com>
2022-09-22 08:26:09 -0700

    changes

--- 0x03-debugging/0-mainc.c
+++ 0x03-debugging/0-mainc.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Test function for positive or negative
- *
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 
@@ -12,5 +12,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
