cc7f0f5421618aaad4bd8e53ab8ef08de7900dab
Elocodes <elochukwuodoeze@gmail.com>
2022-09-28 16:52:10 -0700

    str_len

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_strlen_recursion - a function thaat returns len of string
+ *@s: string to be checked
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int string_length;
+
+	string_length = strlen(s);
+	return (string_length);
+}
