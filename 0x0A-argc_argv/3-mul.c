efa10c9d853087a38526b0761e12c0bf36c31c59
Elocodes <elochukwuodoeze@gmail.com>
2022-10-02 22:37:09 -0700

    multiplies argv 1 and 2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,20 +11,17 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
-	int multiplication;
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	multiplication = a * b;
-
-	if (argc == 1 || argc > 3)
+	if (argc == 1 || argc == 2 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		printf("%d\n", multiplication);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
 	}
 	return (0);
 }
