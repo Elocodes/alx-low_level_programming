49b6c8d34611a02087788386a277d4ccb3801779
Elocodes <elochukwuodoeze@gmail.com>
2022-10-06 14:59:29 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
 
+/**
+ **malloc_checked - function allocates memory
+ *@b: input
+ *
+ * Return: returns a pointer to the memory space
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *j = malloc(b);
