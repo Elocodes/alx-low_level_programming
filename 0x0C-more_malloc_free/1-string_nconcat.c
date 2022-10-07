1dff663fd83f9b012b0d2a72bcf0fe44ec23a64d
Elocodes <elochukwuodoeze@gmail.com>
2022-10-07 16:33:38 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= strlen(s2))
 		n = strlen(s2);
 
-	mem = malloc(sizeof(mem) * (strlen(s1) + n + 1));
+	mem = malloc(sizeof(*mem) * (strlen(s1) + n + 1));
 	if (mem == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
