760919bf33974be96428d6ec78ab1b7f1740161e
Elocodes <elochukwuodoeze@gmail.com>
2022-10-10 15:29:31 -0700

    filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function prints the file name
+ * as defined in the macro file
+ *
+ * Return: returns 0, success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
