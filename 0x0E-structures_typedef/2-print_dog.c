96b6d9924b5e10e58ebec70a2a1f87d2fde77c19
Elocodes <elochukwuodoeze@gmail.com>
2022-10-11 16:15:07 -0700

    printing structs values

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,8 +19,8 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		if (d->age < 1)
-			printf("Age: (nil)\n");
+		if (d->age == 0)
+			printf("Age: %f\n", 0);
 		else
 			printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
