5bc828a2d553246a9eb809c982de1da425754504
Elocodes <elochukwuodoeze@gmail.com>
2022-10-11 16:05:35 -0700

    printing struct updates from funct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_dog - function prints the info of dog and dog owner
+ *@d: a pointer to struct dog contained in the header file
+ *it points to the values; name, age and owner to be printed as
+ *passed by the main function
+ *@name - dog name as passed in the main function
+ *@age - age as passed in the main function
+ *@owner - owners name as passed in the main function
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age < 1)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
+
