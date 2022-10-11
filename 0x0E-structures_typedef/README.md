6e93ba8896f73f67b1488a4f86e28f0dceaa549e
Elocodes <elochukwuodoeze@gmail.com>
2022-10-10 21:57:12 -0700

    using structures

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+descriptions go here
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog is of the type structure
+ *@name: owners name
+ *@age: owners age
+ *@owner: other info
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
