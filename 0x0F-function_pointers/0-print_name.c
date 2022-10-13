fee3b821c1221e8b58344e476c88ea1026de3e45
Elocodes <elochukwuodoeze@gmail.com>
2022-10-12 17:56:41 -0700

    function call

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_name - function calls the function that prints name
+ *@name: name as passed by main function
+ *@f: pointer to the print function
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
