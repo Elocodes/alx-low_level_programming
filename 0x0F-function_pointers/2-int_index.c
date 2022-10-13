77ec36fcb671174903437041476cfd5087c201b8
Elocodes <elochukwuodoeze@gmail.com>
2022-10-12 20:42:13 -0700

    array compare values

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+/**
+ * int_index - function compares array values
+ *@array: array
+ *@size: size of array
+ *@cmp: pointer to array value function
+ *
+ * Return: returns the first index of value if not
+ * matched with array value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
