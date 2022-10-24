4a1750c74d6fab4ce1c10908046ece5a3d6ab875
Elocodes <elochukwuodoeze@gmail.com>
2022-10-23 23:08:00 -0700

    return num of element in list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints all the elements of a list
+ *@h: pointer to the struct containing the elements
+ * struct is defined in list.h
+ * Return: returns the number of nodes in the list
+ *
+ * Description - if NULL is passed to the print list function,
+ * nothing is returned, else, the while loop increments the number of
+ * nodes passed.
+ * it repeats loop until h meets the node that ends
+ * the linked list with pointer part being null
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->n == 0)
+		{
+			return (-1);
+		}
+		else
+		{
+			count += 1;
+		}
+		h = h->next;
+	}
+	return (count);
+}
