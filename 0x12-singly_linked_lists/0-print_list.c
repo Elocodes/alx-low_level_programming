e83a7f5d984ca8fa7823deebafa54180a8a773f4
Elocodes <elochukwuodoeze@gmail.com>
2022-10-23 17:10:26 -0700

    printing linked list data

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,12 +5,32 @@
  *@h: pointer to the struct containing the elements
  * struct is defined in list.h
  * Return: returns the number of element printed
+ *
+ * Description - if NULL is passed to the print list function,
+ * nothing is returned, else, the while loop prints the data in the node
+ * provided its pointer part is not null.
+ * it repeats loop until h meets the node that ends
+ * the linked list with pointer part being null
  */
 size_t print_list(const list_t *h)
 {
-	if (h->str == NULL)
-		printf("[%d] %s\n", 0, "(nil)");
-	else
-		printf("[%d] %s\n", h->len, h->str);
-	return (1);
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
