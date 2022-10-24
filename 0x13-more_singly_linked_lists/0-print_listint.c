a12a54c08d5fa6792e7e74694c103997d06ddcf3
Elocodes <elochukwuodoeze@gmail.com>
2022-10-23 22:59:25 -0700

    printimg list data, integer

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list
+ *@h: pointer to the struct containing the elements
+ * struct is defined in list.h
+ * Return: returns the number of element printed
+ *
+ * Description - if NULL is passed to the print list function,
+ * nothing is returned, else, the while loop prints the data in the node
+ * provided its pointer part is not null.
+ * it repeats loop until h meets the node that ends
+ * the linked list with pointer part being null
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->n == 0)
+		{
+			printf("%s\n", "(noData)");
+		}
+		else
+		{
+			printf("%d\n", h->n);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
