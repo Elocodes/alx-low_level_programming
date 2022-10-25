fad8a1a99559429e05ad5e8d4c6656b7b2ec66e4
Elocodes <elochukwuodoeze@gmail.com>
2022-10-24 23:40:49 -0700

    add node at end of list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint_end - function adds a new node to the end
+ * of struct listint_t
+ *@head: pointer to the pointer of the node element
+ *@n: the data of the new node
+ *
+ * Return: returns the address of the new node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	listint_t *ptr2last = *head;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	/**
+	 * Description - for easy visualization, consider
+	 * each node appended after head as the last attached
+	 * head-file. if this file meets an empty head, it
+	 * simply becomes the head. if not empty, the last
+	 * head's next stores the address of the new node
+	 * that is becomes a pointer to it
+	 */
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	while (ptr2last->next != NULL)
+	{
+		ptr2last = ptr2last->next;
+	}
+	ptr2last->next = new_node;
+	return (ptr2last);
+}
