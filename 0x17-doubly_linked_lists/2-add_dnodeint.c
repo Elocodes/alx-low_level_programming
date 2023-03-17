fa358a18a9d8c76a4354e403123c6316c3c9ffc7
Elocodes <elochukwuodoeze@gmail.com>
2023-03-16 19:03:08 -0700

    linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,6 +12,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node, *temp;
 
+	if (head == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
@@ -37,5 +40,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	temp->prev = new_node;
 	*head = new_node;
-	return (*head);
+	return (new_node);
 }
